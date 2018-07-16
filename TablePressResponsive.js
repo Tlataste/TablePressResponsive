ad09d4e66f447ad5b81d5d1f4214fe3fd1b93465
tlataste <tlataste@macbook-pro-de-tlataste.home>
2018-07-16 20:56:17 +0200

    first commit

--- TablePressResponsive.js
+++ TablePressResponsive.js
@@ -1,20 +1,26 @@
 $(document).ready(function()){
 
-    var thead = [];
+    if($(window).width() <= 600){
+
+        var thead = [];
 
 
-    $('.tablepress th').each(function() {
-        thead.push($(this).text());
-    });
-
-    $('.tablepress tr').each(function() {
-        index = 0;
-        $(this).attr('data-tr', $(this).children('td:first-child').text());
-        $(this).children('td').each(function(){
-            $(this).attr('data-th', thead[index]);
-            index++
+        $('.tablepress th').each(function() {
+            thead.push($(this).text());
         });
-    });
+
+        $('.tablepress tr').each(function() {
+            index = 0;
+            $(this).attr('data-tr', $(this).children('td:first-child').text());
+            $(this).children('td').each(function(){
+                $(this).attr('data-th', thead[index]);
+                index++
+            });
+        });
+
+    }
+
+
 
 
 }
